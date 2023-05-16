e24a65dd71d69c24803aaf3037926ff1adeb6d63
jasonkornegay <jason.kornegay@fox.com>
2023-05-16 14:31:08 -0400

    Add project files.

---
+++ EmpowerSession_01_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerSession_01_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerSession_01_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("97614923-188D-494B-99D7-21E49AF72E72")]
+[assembly: AssemblyVersion("1.0.0.0")]
